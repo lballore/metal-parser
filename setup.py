d0a181c5187eea7dbdcc9b799b18b62addc0b48e
Luca Ballore <luca.ballore@gmail.com>
2019-12-26 18:32:22 +0100

    NOISSUE - Add __init__.py where missing and update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='metalparser',
-    version='0.1.2-beta',
+    version='0.1.3-beta',
     description='Python library for heavy metal song lyrics, albums, song titles and other info.',
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
